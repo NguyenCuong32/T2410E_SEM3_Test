81336921204321db0f239dc2a52b543d281678a8
Mona Lisa <YOUR_EMAIL>
2026-03-05 20:19:43 +0700

    nop bai DAMS

---
+++ FunctionApp1/CreateAssetFunction.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using FunctionApp1.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+public class CreateAssetFunction
+{
+    [Function("createasset")]
+    public async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "createasset")] HttpRequest req)
+    {
+        try
+        {
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var data = JsonConvert.DeserializeObject<AssetModel>(requestBody);
+
+            if (data == null || string.IsNullOrEmpty(data.AssetName))
+            {
+                return new BadRequestObjectResult("Invalid data");
+            }
+            string connectionString =
+         "Data Source=HAQUYNH\\SQLEXPRESS;Initial Catalog=BATTLEGAMEV1;User ID=sa;Password=Quynh2006@;TrustServerCertificate=True";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                string query = @"INSERT INTO Asset (AssetName, LevelRequire)
+                                 VALUES (@AssetName, @LevelRequire)";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@AssetName", data.AssetName);
+                    cmd.Parameters.AddWithValue("@LevelRequire", data.LevelRequire);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            return new OkObjectResult(new
+            {
+                message = "Asset created successfully"
+            });
+        }
+        catch (Exception ex)
+        {
+            return new BadRequestObjectResult(new
+            {
+                error = ex.Message
+            });
+        }
+    }
+}
---
+++ FunctionApp1/RegisterPlayerFunction.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using FunctionApp1.Model;
+using System.Data.SqlClient;
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Newtonsoft.Json;
+
+public class RegisterPlayerFunction
+{
+    [Function("registerplayer")]
+    public async Task<HttpResponseData> Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequestData req)
+    {
+        string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+
+        var data = JsonConvert.DeserializeObject<PlayerModel>(requestBody);
+
+        string connectionString = "Server=localhost\\SQL2025;Database=BATTLEGAME;Trusted_Connection=True;TrustServerCertificate=True;";
+        using (SqlConnection conn = new SqlConnection(connectionString))
+        {
+            conn.Open();
+
+            string query = @"INSERT INTO Player(PlayerName,FullName,Age,Level,Email)
+                             VALUES(@PlayerName,@FullName,@Age,@Level,@Email)";
+
+            SqlCommand cmd = new SqlCommand(query, conn);
+
+            cmd.Parameters.AddWithValue("@PlayerName", data.PlayerName);
+            cmd.Parameters.AddWithValue("@FullName", data.FullName);
+            cmd.Parameters.AddWithValue("@Age", data.Age);
+            cmd.Parameters.AddWithValue("@Level", data.Level);
+            cmd.Parameters.AddWithValue("@Email", data.Email);
+
+            cmd.ExecuteNonQuery();
+        }
+
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        await response.WriteStringAsync("Player created successfully");
+
+        return response;
+    }
+}
